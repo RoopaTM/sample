c43a8d2ecc49ac5cd7f870bb348d910504a5bb6c
roopatm <roopatm6@gmail.com>
2022-07-28 12:41:18 -0400

    hh

---
+++ src/main/java/com/tyss/Copy2.java
@@ -0,0 +1,13 @@
+package com.tyss;
+
+public class Copy2 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("heloo");
+		System.out.println("hiii");
+
+	}
+
+}
